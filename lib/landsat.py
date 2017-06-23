d2c76fdede34b3feee9833e8f5555dd30ff08a1a
maxgrossman <m.grossman1813@gmail.com>
2017-06-23 14:05:17 -0400

    download via landsat.txt
    now scenes are downloaded via landsat.txt
    it's slowish

--- lib/landsat.py
+++ lib/landsat.py
@@ -1,11 +1,17 @@
 # libraries
 from tempfile import mkdtemp
 from sdownloader import Landsat8
-import sys
-# tmpfolder and set it as our downloads folder
-temp_folder = mkdtemp()
-l = Landsat8(download_dir = temp_folder)
-# download provided scenes
-scenes = l.download(['LC81670712014347LGN00'], bands=[4,3,2])
-# show me it happened
-[s.name for s in scenes]
+# create disaster scene groups
+landsat_file = open('landsat.txt', 'r')
+disaster_groups = [
+    line.strip('\n').split(',')
+    for line in landsat_file.readlines()
+]
+
+# process each pre-post file grouping
+for group in disaster_groups:
+    temp_folder = mkdtemp()
+    l = Landsat8(download_dir = temp_folder)
+    # download provided scenes to temp
+    scenes = l.download(group[1:], bands=[4,3,2])
+    print("downloaded: " + [s.name for s in scenes])
