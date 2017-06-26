2950e9de14847751d6d1dbb00d54e0740b065135
maxgrossman <m.grossman1813@gmail.com>
2017-06-26 15:09:46 -0400

    contrast enhancement implemented
    contrast enhancement via autoscaling nir/blue and green bands differently (the key is to make the soil less pronounced as it is what makes the water so bright)

--- lib/landsat.py
+++ lib/landsat.py
@@ -2,7 +2,8 @@
 from tempfile import mkdtemp
 from sdownloader import Landsat8
 from gippy import GeoImage
-# create disaster scene groups
+import numpy as np
+#cdisaster_scenes = l.download( create disaster scene groups
 landsat_file = open('landsat.txt', 'r')
 disaster_groups = [
     line.strip('\n').split(',')
@@ -13,9 +14,9 @@ for group in disaster_groups:
     temp_folder = mkdtemp()
     l = Landsat8(download_dir = temp_folder)
     # download provided scenes to temp
-    disaster_scenes = l.download(group[1:], bands=[4,3,2])
+    disaster_scenes = l.download(group[1:], bands=[5,4,3,])
     print("downloaded: " + str([s.name for s in disaster_scenes]))
-    # make GeoImage from downed bands
+    #ndsat8(download_dir = temp_folder) make GeoImage from downed bands
     for idx,itm in enumerate(disaster_scenes.scenes):
         disaster_files = disaster_scenes[idx].files
         # bands are in order of r,g,b
@@ -26,3 +27,12 @@ for group in disaster_groups:
             nodata=0
         )
         print("Geoimg generated")
+        # contrast enhancement:
+            # clip last 5% of green , and 10% of nir/blue dn values, then rescale them
+        print("Histogram Equalization for bands")
+        for bandnum, band in enumerate(scene_geoimg.bandnames()):
+            color = scene_geoimg.bandnames()[bandnum]
+            if color == 'green':
+                scene_geoimg[color] = scene_geoimg[color].autoscale(0,255,percent=5.0)
+            else:
+                scene_geoimg[color] = scene_geoimg[color].autoscale(0,255,percent=10.0)
