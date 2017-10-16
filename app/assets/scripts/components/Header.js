c92f4d65284e4fe5f2f08307d202130e2fad44ea
maxgrossman <m.grossman1813@gmail.com>
2017-10-16 12:06:46 -0400

    make nav color page specific

--- app/assets/scripts/components/Header.js
+++ app/assets/scripts/components/Header.js
@@ -1,16 +1,22 @@
 'use strict';
 
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import c from 'classnames';
 
 class Header extends Component {
+  static propTypes = {
+    history: PropTypes.object.isRequired
+  }
   render () {
+    const headerClass = c({ 'header_main': this.props.history.location.pathname === '/' });
     return (
       <div>
         <nav className='page__nav'>
           <div className='inner'>
             <Link to="/" className='page__title'>Historic Disaster Explorer</Link>
-            <ul>
+            <ul className={headerClass}>
               <li>
                 <Link to="/disasters">Historic Disasters</Link>
               </li>
