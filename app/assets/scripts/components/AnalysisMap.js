bafacbe510b9d767199cc0256374050508cc43c0
maxgrossman <m.grossman1813@gmail.com>
2017-10-16 15:51:19 -0400

    add pagination

--- app/assets/scripts/components/AnalysisMap.js
+++ app/assets/scripts/components/AnalysisMap.js
@@ -24,19 +24,19 @@ class AnalysisMap extends Component {
     disaster: PropTypes.object.isRequired,
     visibleLayer: PropTypes.object.isRequired
   }
-  _loadLayers () {
+  _loadLayers (props) {
     // base layer
-    if (this.props.disaster.dmetric) {
-      let id = `${config.mapLayers[this.props.disaster.dmetric].id}-${this.props.disaster.c}`;
-      let footprintSource = makeFootPrintSource(this.props.disaster);
+    if (props.disaster.dmetric) {
+      let id = `${config.mapLayers[props.disaster.dmetric].id}-${props.disaster.c}`;
+      let footprintSource = makeFootPrintSource(props.disaster);
       this._map.addSource(id, footprintSource);
-      this._map.addLayer(makeFootPrintLayer(this.props.disaster, id));
+      this._map.addLayer(makeFootPrintLayer(props.disaster, id));
     }
     // add & hide overlay layers
     Object.keys(config.mapLayers['exposure-loss'].layers.ids).forEach((key) => {
       // add layer
       let layerIdBase = config.mapLayers['exposure-loss'].layers.ids[key];
-      this._map.addLayer(makeExposureLayer(this.props.disaster, layerIdBase));
+      this._map.addLayer(makeExposureLayer(props.disaster, layerIdBase));
       // hide layer
       let layerId = `${config.mapLayers['exposure-loss'].id}-${layerIdBase}`;
       if (this._map.getLayer(layerId)) {
@@ -48,27 +48,56 @@ class AnalysisMap extends Component {
     this._map.addControl(new mapboxgl.NavigationControl(), 'bottom-right');
   }
   componentDidMount () {
-    console.log(this.props);
     mapboxgl.accessToken = config.mapboxApiKey;
     this._map = new mapboxgl.Map({
       container: 'analysisMap',
       style: config['disaster-data']
-    }).fitBounds(this.props.disaster.bbox, {
-      animate: false,
-      padding: config.boundsPadding
     });
     this._map.scrollZoom.disable();
     // remove scroll zoom
     this._addNavigation();
+    this._map.fitBounds(this.props.disaster.bbox, {
+      animate: false,
+      padding: config.boundsPadding
+    });
     // add layers, then hide them
     this._map.on('load', () => {
-      this._loadLayers();
+      this._loadLayers(this.props);
     });
+    // // get layer ids matching id from current visibleLayer
+    // let selectedIds = Object.keys(config.mapLayers['exposure-loss'].layers.ids)
+    //   .filter((k) => {
+    //     const layerRegEx = RegExp(this.props.visibleLayer.layer);
+    //     const configLayer = config.mapLayers['exposure-loss'].layers.ids[k];
+    //     return layerRegEx.test(configLayer);
+    //   });
+    // selectedIds.forEach((id) => {
+    //   let layerId = `${config.mapLayers['exposure-loss'].id}-${config.mapLayers['exposure-loss'].layers.ids[id]}`;
+    //   let layers = this._map.getStyle().layers;
+    //   // find style layer matching current selectedIds id
+    //   let layer = layers.find((l) => {
+    //     return l.id === layerId;
+    //   });
+    //   // grab all visible layers of the opposite id group (i.e if id is admin, grab grid)
+    //   let visibleLayers = getVisibleExposureLayers(layerId, layers, id);
+    //   // only allow making non visible layers visible if no layers found in getVisibleExposureLayers
+    //   if (layer.layout.visibility === 'none' && !visibleLayers) {
+    //     this._map.setLayoutProperty(layer.id, 'visibility', 'visible');
+    //   } else {
+    //     this._map.setLayoutProperty(layer.id, 'visibility', 'none');
+    //   }
+    // });
   }
   componentWillReceiveProps (nextProps) {
+    if (this.props.disaster !== nextProps.disaster) {
+      this._map.fitBounds(nextProps.disaster.bbox, {
+        animate: false,
+        padding: config.boundsPadding
+      });
+    }
     // if the selected layer(s) is not visible, make it so on click
     // also make sure to only allow one layer to be visible at a time.
-
+  
     // get layer ids matching id from current visibleLayer
     let selectedIds = Object.keys(config.mapLayers['exposure-loss'].layers.ids)
     .filter((k) => {
@@ -92,6 +121,7 @@ class AnalysisMap extends Component {
         this._map.setLayoutProperty(layer.id, 'visibility', 'none');
       }
     });
+    // }
   }
   render () {
     return (
--- app/assets/scripts/views/DisasterProfile.js
+++ app/assets/scripts/views/DisasterProfile.js
@@ -3,43 +3,71 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { setOverlayMetric } from '../actions/action-creators';
+import { setOverlayMetric, setDisaster } from '../actions/action-creators';
 
 import AnalysisMap from '../components/AnalysisMap';
 
 class DisasterProfile extends Component {
   constructor (props) {
     super(props);
-    this.parseURL = this.parseURL.bind(this);
     this.makeMetricButtons = this.makeMetricButtons.bind(this);
+    this.nextDisaster = this.nextDisaster.bind(this);
+    // this.nextDisasterLink = this.nextDisasterLink.bind(this);
+    // this.nextDisasterText = this.nextDisasterText.bind(this);
+    this.makeNextDisaster = this.makeNextDisaster.bind(this);
+    this.renderDisasterProfile = this.renderDisasterProfile.bind(this);
   }
   static propTypes = {
     disasters: PropTypes.array.isRequired,
+    disaster: PropTypes.object.isRequired,
     match: PropTypes.object.isRequired,
+    history: PropTypes.object.isRequired,
+    _setDisaster: PropTypes.func.isRequired,
     _setOverlayMetric: PropTypes.func.isRequired
   }
-  // uses route url to query disasters list for matching disaster
-  parseURL () {
-    const disastersInfo = this.props.match.url.split('/disasters/')[1].split('-');
-    this.disaster = this.props.disasters.find(d => d.c === disastersInfo[0]);
+  componentWillMount () {
+    let disasterInfo = this.props.match.url.split('/disasters/')[1].split('-');
+    let disaster = this.props.disasters.find(d => d.c === disasterInfo[0]);
+    disaster.index = this.props.disasters.findIndex(d => d.c === disasterInfo[0]);
+    this.props._setDisaster(disaster);
   }
   makeMetricButtons () {
     return ['Exposure', 'Annualized Loss', 'Loss Ratio'].map((m, i) => {
       return (
-        <li><button className='button button--large button--base-bounded' key={i}
+        <li><button className='button button--large button--base-bounded' key={m}
           value={m.replace(' ', '-').toLowerCase()}
           onClick={(e) => { this.props._setOverlayMetric(e.target.value); }}>{m}</button></li>
       );
     });
   }
-  render () {
-    this.parseURL();
+
+  nextDisaster () {
+    const isLastDisaster = (this.props.disaster.index === this.props.disasters.length - 1);
+    return !isLastDisaster ? this.props.disasters[this.props.disaster.index + 1] : this.props.disasters[0];
+  }
+
+  nextDisasterLink (nextDisaster) {
+    return `/disasters/${nextDisaster.c}-${nextDisaster.y}-${nextDisaster.t.toLowerCase()}`;
+  }
+
+  nextDisasterText (nextDisaster) {
+    return `${nextDisaster.n} ${nextDisaster.y} ${nextDisaster.t}`;
+  }
+
+  makeNextDisaster () {
+    const isLastDisaster = (this.props.disaster.index === this.props.disasters.length - 1);
+    let nextDisaster = !isLastDisaster ? this.props.disasters[this.props.disaster.index + 1] : this.props.disasters[0];
+    nextDisaster.index = !isLastDisaster ? this.props.disaster.index + 1 : 0;
+    this.props._setDisaster(nextDisaster);
+  }
+
+  renderDisasterProfile () {
     return (
       <div>
         <section className='inpage__header'>
           <div className='inner'>
-            <p className='subheading'>{this.disaster.m} {this.disaster.y}</p>
-            <h1 className='heading--xxlarge'>{this.disaster.n} {this.disaster.y} {this.disaster.t}</h1>
+            <p className='subheading'>{this.props.disaster.m} {this.props.disaster.y}</p>
+            <h1 className='heading--xxlarge'>{this.props.disaster.n} {this.props.disaster.y} {this.props.disaster.t}</h1>
             <hr align='left'></hr>
             <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed nisl augue. Morbi condimentum tempor ornare. Sed rutrum pretium accumsan. Duis iaculis consequat nunc a tempu</p>
           </div>
@@ -109,7 +137,7 @@ dolor si  t amet, consectetur adipiscing elit. Duis sed nisl augue</p>
               </ul>
             </div>
             <div>
-             <AnalysisMap disaster={this.disaster} />
+             <AnalysisMap disaster={this.props.disaster} />
             </div>
           </section>
           <section className='images'>
@@ -122,26 +150,34 @@ dolor si  t amet, consectetur adipiscing elit. Duis sed nisl augue</p>
           </section>
         </section>
         <section className='inpage__footer'>
-          <div className='inner'>
+          <div className='inner' >
             <h2 className='alt-heading'>Next</h2>
-            <h1 className='heading--xlarge'> Pakistan Floods 2010</h1>
-            <a className='link--primary-light' href=''>View Case Study</a>
+            <h1 className='heading--xlarge'>{this.nextDisasterText(this.nextDisaster())}</h1>
+            <a href='' onClick={(e) => { e.preventDefault(); this.makeNextDisaster(); this.props.history.replace(this.nextDisasterLink()); }} className='link--primary-light'>
+              View Case Study
+            </a>
           </div>
         </section>
       </div>
     );
   }
+
+  render () {
+    return this.props.disaster.set ? this.renderDisasterProfile() : (<div/>);
+  }
 }
 
 const selector = (state) => {
   return {
-    disasters: state.disasters
+    disasters: state.disasters,
+    disaster: state.disaster
   };
 };
 
 const dispatcher = (dispatch) => {
   return {
-    _setOverlayMetric: (metric) => dispatch(setOverlayMetric(metric))
+    _setOverlayMetric: (metric) => dispatch(setOverlayMetric(metric)),
+    _setDisaster: (disaster) => dispatch(setDisaster(disaster))
   };
 };
 
