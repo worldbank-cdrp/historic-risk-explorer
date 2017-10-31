e8933561bca9cafa41e0d6c948df6b02e6d4b6dc
maxgrossman <m.grossman1813@gmail.com>
2017-10-31 09:08:08 -0400

    add static mapbox image for disasters w/out image

--- app/assets/scripts/utils/disaster.js
+++ app/assets/scripts/utils/disaster.js
@@ -1,5 +1,8 @@
 'use strict';
 
 export function makeImage (disaster) {
-  return { 'backgroundImage': [ `linear-gradient(rgba(0, 0, 0, 0.7),rgba(0, 0, 0, 0.7))`, `url('/assets/graphics/content/${disaster.image}')` ] };
+  if (disaster.image) {
+    return { 'backgroundImage': [ `linear-gradient(rgba(0, 0, 0, 0.7),rgba(0, 0, 0, 0.7))`, `url(assets/graphics/content/${disaster.image}` ] };
+  }
+  return { 'backgroundImage': [ `linear-gradient(rgba(0, 0, 0, 0.7),rgba(0, 0, 0, 0.7))`, `url('https://api.mapbox.com/styles/v1/wbg-cdrp/cj7w85rme3rvm2ro19y4q5pss/static/${disaster.staticXYZ.toString()}/1280x600@2x?access_token=pk.eyJ1Ijoid2JnLWNkcnAiLCJhIjoiY2l1Z3pxZDVwMDBxcDMzcDJjYmRpYnBicSJ9.hjlLP5TEVhqbTwzhFA1rZw'` ] };
 }
